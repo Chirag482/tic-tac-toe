0843a0e644b74042b4b242ab0e871f9550fb93ae
Chirag <chiragvalecha97@gmail.com>
2021-04-27 13:12:13 +0530

    add simple play functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Game from "./Game";
+import Moves from "./Moves";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={styles.heading}>Tic Tac Toe</h1>
+        <div className="main">
+          <Game />
+          <Moves />
+        </div>
+      </div>
+    );
+  }
 }
+
+const styles = {
+  heading: {
+    textAlign: "center",
+    background: "#4367b2",
+    height: 70,
+    marginTop: 0,
+    color: "white",
+    paddingTop: 15,
+  },
+};
 
 export default App;
---
+++ src/Moves.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Moves extends React.Component {
+  render() {
+    return (
+      <div className="moveslist">
+        <h1>Moves</h1>
+      </div>
+    );
+  }
+}
+
+export default Moves;
